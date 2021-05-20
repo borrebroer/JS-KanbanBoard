800a5a32898444861924ebf7b75f9a2b5544f467
Borre Broer <borre@use.startmail.com>
2021-05-20 08:08:27 +0200

    Test module 13

--- js/kanban.js
+++ js/kanban.js
@@ -43,5 +43,5 @@ const create_item = () => {
 };
 
 document.querySelectorAll('.drop').forEach(element => {
-  element.addEventListener('drop', event =>{event.preventDefault(); event.dataTransfer.getData('text');},true)
+  element.addEventListener('drop', event =>{event.preventDefault(); const id = event.dataTransfer.getData('text');},true)
 });
